fe552808b052f4d314402124b18edfc35e220864
vivekadi <vivek.adishesha@gmail.com>
2020-02-02 18:45:43 +0530

    seven segment

---
+++ arm_lab/ALS-SDA-ARMCTXM3-06/code/sev_seg/seven_seg.d
@@ -0,0 +1,7 @@
+.\seven_seg.o: Seven_seg.c
+.\seven_seg.o: C:\Keil4\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+.\seven_seg.o: C:\Keil4\ARM\CMSIS\Include\core_cm3.h
+.\seven_seg.o: C:\Keil4\ARM\ARMCC\bin\..\include\stdint.h
+.\seven_seg.o: C:\Keil4\ARM\CMSIS\Include\core_cmInstr.h
+.\seven_seg.o: C:\Keil4\ARM\CMSIS\Include\core_cmFunc.h
+.\seven_seg.o: C:\Keil4\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
