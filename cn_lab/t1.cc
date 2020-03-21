a4125b0ec68dadb7c4a13754814be1582e13a0c0
vivekadi <vivek.adishesha@gmail.com>
2020-03-22 01:51:16 +0530

    uptade t1.cc
    Co-Authored-By: keshavbharadwaj <keshavbharadwaj@users.noreply.github.com>

--- cn_lab/t1.cc
+++ cn_lab/t1.cc
@@ -1,5 +1,5 @@
 
-#include<ns3/core-module.h>
+#include<ns3/core-module.h>                //***** Remember these packages as C-PAINT
 #include<ns3/point-to-point-module.h>
 #include<ns3/network-module.h>
 #include<ns3/internet-module.h>
